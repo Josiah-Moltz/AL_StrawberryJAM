0ad78cb7726b92d5f653f1ac6ba73ec2d4be7216
Melody Lew <73132847+melodylew@users.noreply.github.com>
2021-12-13 16:43:35 -0500

    Update OrderedArrayList.java

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,34 +1,101 @@
+// Clyde "Thluffy" Sinclair
+// APCS pd00
+// Lab02
+// 2021-12-13
+
+/**********************************************
+ * class OrderedArrayList
+ * wrapper class for ArrayList
+ * maintains invariant that elements are ordered (ascending)
+ * (SKELETON)
+ **********************************************/
+
+
 import java.util.ArrayList;
 
-public class OrderedArrayList {
-  public ArrayList<Integer> _data;
+public class OrderedArrayList
+{
 
+  // instance of class ArrayList, holding objects of type Integer
+  // (i.e., objects of a class that implements interface Integer)
+  private ArrayList<Integer> _data;
+
+  // default constructor
+  // initializes instance variables
   public OrderedArrayList() {
     _data = new ArrayList<Integer>();
   }
 
-  public boolean add(int i) {
-    int index = 0;
-    while (index < size() && i > get(index)) {
-      index += 1;
-    }
-    _data.add(index, i);
-    return true;
+  public String toString()
+  {
+    return _data.toString();
   }
 
-  public int remove(int index) {
-    return _data.remove(index);
+  public Integer remove( int i )
+  {
+    return _data.remove(i);
   }
 
-  public int get(int index) {
-    return _data.get(index);
-  }
-
-  public int size() {
+  public int size()
+  {
     return _data.size();
   }
 
-  public String toString() {
-    return _data.toString();
+  public Integer get( int i )
+  {
+    return _data.get(i);
   }
-}
+
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a linear search to find appropriate index
+  public void addLinear(Integer newVal)
+  {
+    int index = 0;
+    while (index < size() && newVal > get(index)) {
+      index += 1;
+    }
+    _data.add(index, newVal);
+  }
+
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a binary search to find appropriate index
+  public void addBinary(Integer newVal)
+  {
+    System.out.println(newVal); //problem with first val
+    int start = 0;
+    int middle = 0;
+    int finish = size();
+    while ((finish - start) > 1 && size() != 0 ){
+      middle = (start + finish) / 2;
+      if (newVal <= get(middle)){
+        finish = middle;
+      }
+      else {
+        start = middle;
+      }
+    }
+    _data.add(finish, newVal);
+  }
+
+  // main method solely for testing purposes
+  public static void main( String[] args )
+  {
+    OrderedArrayList Franz = new OrderedArrayList();
+
+    // testing linear searchadd(middle, newVal);
+    for( int i = 0; i < 15; i++ )
+      Franz.addLinear( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+    // testing binary search
+    Franz = new OrderedArrayList();
+    for( int i = 0; i < 15; i++ )
+      Franz.addBinary( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+    /*-----v-------move-me-down-----------------v--------
+      =====^====================================^=========*/
+
+  }//end main()
+
+}//end class OrderedArrayList
